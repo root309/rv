a1bb4ed31b8e88511af387f99cd01e1b4196ba4d
root309 <choco00101x@gmail.com>
2024-01-20 14:12:17 +0900

    sub mul div

--- main.c
+++ main.c
@@ -4,17 +4,34 @@
 #include <ctype.h>
 
 void generate_assembly(const char *source) {
-    const char *token = strtok((char *)source, " ");
+    char *token = strtok((char *)source, " ");
+    int register_count = 0;
+
     while (token != NULL) {
         if (isdigit(token[0])) {
-            printf("addi x0, x0, %s\n", token);
+            // For numeric tokens, generate an instruction to load the immediate value into a register
+            printf("li x%d, %s\n", register_count, token);
+            register_count++;
+        } else if (token[0] == '+') {
+            // add
+            printf("add x%d, x%d, x%d\n", register_count - 2, register_count - 2, register_count - 1);
+        } else if (token[0] == '-') {
+            // sub
+            printf("sub x%d, x%d, x%d\n", register_count - 2, register_count - 2, register_count - 1);
+        } else if (token[0] == '*') {
+            // mul
+            printf("mul x%d, x%d, x%d\n", register_count - 2, register_count - 2, register_count - 1);
+        } else if (token[0] == '/') {
+            // div
+            printf("div x%d, x%d, x%d\n", register_count - 2, register_count - 2, register_count - 1);
         }
+        // next token
         token = strtok(NULL, " ");
     }
 }
 
 int main() {
-    char source[] = "3 + 4";
+    char source[] = "3 + 4 - 2 * 5 / 1";
 
     generate_assembly(source);
 
